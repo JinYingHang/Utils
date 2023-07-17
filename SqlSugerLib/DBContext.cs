12e7e43986d28020124055d614a917042bf7b4db
这杯酒叫蒹葭 <103085412+JinYingHang@users.noreply.github.com>
2023-07-17 17:54:04 +0800

    111

---
+++ SqlSugerLib/DBContext.cs
@@ -0,0 +1,62 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utils;
+
+namespace SqlSugarHelper
+{
+    public class DbContext<T> where T : class, new()
+    {
+        //用来处理事务多表查询和复杂的操作
+        public SqlSugarClient Db;
+
+        //用来处理事务多表查询和复杂的操作
+        public SimpleClient<T> CurrentDb { get { return new SimpleClient<T>(Db); } }
+
+        public DbContext() {
+            try {
+                Db = new SqlSugarClient(new ConnectionConfig() {
+                    ConnectionString = Config.ReadConfigValue("ConnectionString"),
+                    DbType = (DbType)int.Parse(Config.ReadConfigValue("DbType")),
+                    InitKeyType = InitKeyType.Attribute,
+                    IsAutoCloseConnection = true,
+                });
+                Db.Aop.OnLogExecuting = (sql, pars) => {
+                    Console.WriteLine(sql + "\r\n" + Db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value)));
+                };
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+
+        public virtual List<T> GetList() {
+            try {
+                return CurrentDb.GetList();
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+        public virtual bool Delete(dynamic id) {
+            try {
+                return CurrentDb.Delete(id);
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+        public virtual bool Update(T obj) {
+            try {
+                return CurrentDb.Update(obj);
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+    }
+}
--- SqlSugerLib/Tools/Config.cs
+++ SqlSugerLib/Tools/Config.cs
@@ -18,26 +18,7 @@ namespace Utils
             string value = "";
             try {
                 ExeConfigurationFileMap map = new ExeConfigurationFileMap();
-                map.ExeConfigFilename = @"Solution Item/autoof.config";
-                Configuration config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
-                value = config.AppSettings.Settings[keyName].Value;
-                return value;
-            }
-            catch (Exception) {
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// 根据KeyName读取指定位置config文件值
-        /// </summary>
-        /// <param name="keyName"></param>
-        /// <returns></returns>
-        public static string ReadConfigValue(string keyName,string filePath) {
-            string value = "";
-            try {
-                ExeConfigurationFileMap map = new ExeConfigurationFileMap();
-                map.ExeConfigFilename = filePath;
+                map.ExeConfigFilename = @"Solution Item/DBContext.config";
                 Configuration config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
                 value = config.AppSettings.Settings[keyName].Value;
                 return value;
