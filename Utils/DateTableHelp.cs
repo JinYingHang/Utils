bd49a4143f8103531aeec2aff9a54dbce2b01b7f
这杯酒叫蒹葭 <103085412+JinYingHang@users.noreply.github.com>
2024-02-21 11:03:27 +0800

    1111

---
+++ Utils/DateTableHelp.cs
@@ -0,0 +1,107 @@
+﻿using ExcelDataReader;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace Utils
+{
+    public static class DataTableHelp
+    {
+        /// <summary>
+        /// .XLS||.XLSX转换成DataSet
+        /// </summary>
+        /// <param name="fileNmaePath"></param>
+        /// <returns></returns>
+        public static DataSet ParseExcelToDataSet(string fileNmaePath) {
+            try {
+                FileStream stream = null;
+                IExcelDataReader excelReader = null;
+                DataSet dataSet = null;
+                try {
+                    //stream = File.Open(fileNmaePath, FileMode.Open, FileAccess.Read);
+                    stream = new FileStream(fileNmaePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                }
+                catch {
+                    return null;
+                }
+                string extension = Path.GetExtension(fileNmaePath);
+                if (extension.ToUpper() == ".XLS") {
+                    excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                else if (extension.ToUpper() == ".XLSX") {
+                    excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+                else {
+                    return null;
+                }
+                //dataSet = excelReader.AsDataSet();//第一行当作数据读取
+                dataSet = excelReader.AsDataSet(new ExcelDataSetConfiguration() {
+                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration() {
+                        UseHeaderRow = true
+                    }
+                });//第一行当作列名读取
+                excelReader.Close();
+                return dataSet;
+            }
+            catch (System.Exception) {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// CSV转换成DataTable
+        /// </summary>
+        /// <param name="fileNmaePath"></param>
+        /// <returns></returns>
+        public static DataTable ParseCSVToDataTable(string fileNmaePath) {
+            try {
+                // 假设你有一个从 CSV 文件中读取数据的方法
+                List<string[]> csvData = ReadCSVFile(fileNmaePath);
+                if (csvData != null && csvData.Any()) {
+                    // 创建 DataTable
+                    DataTable dataTable = new DataTable();
+
+                    // 使用第一行数据作为列名
+                    string[] headers = csvData[0];
+                    foreach (string header in headers) {
+                        dataTable.Columns.Add(new DataColumn(header));
+                    }
+
+                    // 添加数据到 DataTable
+                    for (int i = 1; i < csvData.Count; i++) {
+                        DataRow row = dataTable.NewRow();
+                        row.ItemArray = csvData[i];
+                        dataTable.Rows.Add(row);
+                    }
+                    return dataTable;
+                }
+                else {
+                    return null;
+                }
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+        // 读取 CSV
+        private static List<string[]> ReadCSVFile(string filePath) {
+            List<string[]> data = new List<string[]>();
+            try {
+                using (StreamReader sr = new StreamReader(filePath)) {
+                    string line;
+                    while ((line = sr.ReadLine()) != null) {
+                        string[] values = line.Split(','); 
+                        data.Add(values);
+                    }
+                }
+            }
+            catch (Exception) {
+                throw;
+            }
+            return data;
+        }
+    }
+}
---
+++ Utils/ScriptUtil.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Utils
+{
+    public class ScriptUtil
+    {
+        public string ReplaceKeys(string script) {
+            // 定义一个正则表达式模式来匹配标记
+            string pattern = @"\^([^']+?)\^";
+            MatchCollection matches = Regex.Matches(script, pattern);
+            // 遍历匹配项并获取标记中的值
+            foreach (Match match in matches) {
+                string key = match.Groups[1].Value;
+                var fullKey = $"^{key}^";
+                var replaceStr= GetReplaceStr(fullKey);
+                script=script.Replace(fullKey, replaceStr);
+
+            }
+            return script;
+        }
+        private string GetReplaceStr(string key) {
+            key = key.Replace("^","");
+            if (key.Contains('.')) {
+                string[] strings = key.Split('.');
+                if (strings[0].ToString().StartsWith("RowCell")) {
+                    switch (strings[0]) {
+                        case "RowCell":
+                            return $"JsonConvert.DeserializeObject<CellObj>(row[\"{strings[1]}\"].ToString())";
+                        case "RowCell(double)":
+                            return $"double.Parse( JsonConvert.DeserializeObject<CellObj>(row[\"{strings[1]}\"].ToString()).Value)";
+                        default:
+                            return $"关键字匹配失败，程序不包含:{strings[0]}关键字，请联系管理员进行添加";
+                    }
+                }
+                else if (strings[0].ToString().StartsWith("sys")) {
+                    //这里目前没用
+                    switch(strings[0]) {
+                        case "sysAvgCol":
+                            return $"CalcUtil.AvgCol({strings[1]})";
+                        case "sysMax":
+                            return $"CalcUtil.sysMax({strings[1]})";
+                        case "sysMin":
+                            return $"CalcUtil.sysMin({strings[1]})";
+                        default:
+                            return $"关键字匹配失败，程序不包含:{strings[0]}关键字，请联系管理员进行添加";
+                    }
+                }
+                else {
+                    return key;
+                }
+            }
+            else {
+                return key;
+            }
+        }
+    }
+}
